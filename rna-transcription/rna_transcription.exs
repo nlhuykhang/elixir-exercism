daa6673787345bc7c7de101ddc98e3b25506addb
Khang Nguyen-Le <khangnlh@Khangs-MacBook-Pro.local>
2016-08-30 20:28:59 +0700

    RNA transcription

---
+++ rna-transcription/rna_transcription.exs
@@ -0,0 +1,21 @@
+defmodule RNATranscription do
+  @map %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U,
+  }
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna |> Enum.map(fn(x) -> @map[x] end)
+  end
+
+end
